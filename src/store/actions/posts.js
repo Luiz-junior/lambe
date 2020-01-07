a6acb8671f6fc2f59d5c7e1d9dfbc387343bbe1e
luiz-junior <junior.rodrigues0222@gmail.com>
2020-01-07 10:40:41 -0300

    news settings of Firebase Storage to upload images

--- src/store/actions/posts.js
+++ src/store/actions/posts.js
@@ -3,9 +3,21 @@ import { ADD_POST, ADD_COMMENT } from './actionTypes';
 
 export const addPost = post => {
   return dispatch => {
-    axios.post('/posts.json', { ...post })
+    axios({
+      url: 'uploadImage',
+      baseURL: 'https://us-central1-lambe-4017e.cloudfunctions.net',
+      method: post,
+      data: {
+        image: post.image.base64
+      }
+    })
       .catch(err => console.log(err))
-      .then(res => console.log(res.data));
+      .then(response => {
+        post.image = response.data.imageUrl;
+        axios.post('/posts.json', { ...post })
+          .catch(err => console.log(err))
+          .then(res => console.log(res.data));
+      });
   };
   /* return {
     type: ADD_POST,
